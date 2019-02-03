dbc074d8cf63e61cae4cbbe58f9f64cd899432af
azartavakoli <46605903+azartavakoli@users.noreply.github.com>
2019-02-03 10:54:02 +0330

    Make Structure

---
+++ src/App.js
@@ -0,0 +1,35 @@
+ import React from 'react';
+
+
+class App extends React.Component{
+
+    // route = [
+    //     {
+    //         exact:true,
+    //         path:'/',
+    //         Component:Home,
+    //         Layout:Master
+    //     },
+    //     {
+    //         exact:true,
+    //         path:'/Login',
+    //         Component:Login
+    //     },
+    //     {
+    //         exact:true,
+    //         path:'REgister',
+    //         Component:Register
+    //     },
+    //     {
+    //         exact:true,
+    //         Component:NotFound
+    //     }
+    //
+    //     ];
+
+    render () {
+           return <h1> App</h1>
+     }
+ }
+
+ export default App;

3f4602c837dd81b0e76ce9e66de487662b164914
azartavakoli <46605903+azartavakoli@users.noreply.github.com>
2019-02-03 18:17:49 +0330

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,69 @@
- import React from 'react';
 
+//vendor
+ import React from 'react';
+ import {Route,Switch} from 'react-router-dom';
+
+//Pages
+ import Master from './Pages/Master';
+ import Home from './Pages/Home';
+ import Login from './Pages/Login';
+ import Register from './Pages/Register';
+ import NotFound from './Pages/NotFound';
+
+ //Assets
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component{
 
-    // route = [
-    //     {
-    //         exact:true,
-    //         path:'/',
-    //         Component:Home,
-    //         Layout:Master
-    //     },
-    //     {
-    //         exact:true,
-    //         path:'/Login',
-    //         Component:Login
-    //     },
-    //     {
-    //         exact:true,
-    //         path:'REgister',
-    //         Component:Register
-    //     },
-    //     {
-    //         exact:true,
-    //         Component:NotFound
-    //     }
-    //
-    //     ];
+    route = [
+        {
+            exact:true,
+            path:'/',
+            Component:Home,
+            Layout:Master
+        },
+        {
+            exact:true,
+            path:'/login',
+            Component:Login
+        },
+        {
+            exact:true,
+            path:'/register',
+            Component:Register
+        },
+        {
+            exact:true,
+            Component:NotFound
+        }
+
+        ];
 
     render () {
-           return <h1> App</h1>
+           return (
+
+               <Switch>
+                   {this.route.map(({path,Component,exact,Layout}) => (
+                       <Route
+                           key={path || 'NotFound'}
+                           exact={exact}
+                           path={path}
+                           Component={Component}
+                            render = {(props) =>(
+
+                               Layout ? <Layout {...props}>
+                                       <Component {...props}/>
+                                   </Layout>
+                                   :<Component{...props}/>
+                           )}
+                       />
+
+                       ) )
+
+                   }
+
+               </Switch>
+           )
      }
  }
 
---
+++ src/Pages/Master.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+class Master extends React.Component{
+
+
+    render(){
+        // console.log(this.props)
+        return <div> {this.props.children}</div>
+    }
+}
+
+export default Master;
